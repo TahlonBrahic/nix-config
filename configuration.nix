98028a52ee31f2eb6de1a8fd6e0fa06bfc165a83
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-08-18 22:03:34 -0500

    Add OpenSSH capabilities.

--- configuration.nix
+++ configuration.nix
@@ -36,6 +36,18 @@
     xkbVariant = "";
   };
 
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = null;
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "prohibit-password";
+    };
+  };
+
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
