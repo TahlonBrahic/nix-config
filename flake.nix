dabda23f59627601b7aa7587e2bd85300cb9bb1c
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-08-19 20:33:31 -0500

    Add home-manager

--- flake.nix
+++ flake.nix
@@ -1,15 +1,12 @@
 {
-  description = "A very basic flake";
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  inputs.home-manager.url = github:nix-community/home-manager;
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs, ... }@attrs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = attrs;
+      modules = [ ./configuration.nix ];
+    };
   };
 }
