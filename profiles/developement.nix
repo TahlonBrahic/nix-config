c8f9ab9f3431154afae48c12d20ef854e192289d
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-08-23 00:00:00 +0000

    Add developement.nix

---
+++ profiles/developement.nix
@@ -0,0 +1,18 @@
+environment.systemPackages = with pkgs; [
+  podman
+  docker
+  ansible
+  python3
+  vim
+  neovim
+  emacs
+  zsh
+  fish
+  bash
+  powershell
+  rustc
+  cargo
+  go
+  openjdk
+  ghc
+];
