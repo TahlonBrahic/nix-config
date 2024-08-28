cc65b52a84dd3e740d893de7ed99c860e0c4ecad
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-08-27 21:44:52 -0500

    Start zsh config

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,28 +1,6 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }
 {
-  # Import Home Manager modules
-  imports = [ <home-manager/modules/default.nix> ];
-
-  home = {
-    username = "tahlon";
-
-    sessionVariables = {
-      EDITOR = "nvim";
-    };
-
-    programs = {
-      zsh = {
-        enable = true;
-        ohMyZsh = {
-          enable = true;
-        };
-      };
-
-      neovim = {
-        enable = true;
-        plugins = [ pkgs.vimPlugins.gruvbox ];
-      };
-    };
-  };
-}
+  imports = [
+  ];
+};
 
