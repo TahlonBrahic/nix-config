39700e315c37d8cd308569b38f24cde7468dd1ec
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-08-28 21:02:28 -0500

    Fully add Kitty terminal emulator configuration

--- modules/global-pkgs.nix
+++ modules/global-pkgs.nix
@@ -1,6 +1,13 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    curl
+    htop
+  ];
+
   programs.tmux = {
     enable = true;
     clock24 = true;
