8b55a277898d9457e0d941a9764ac37d8447893c
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-09-02 18:29:47 -0500

    Adjust home-manager

--- home/default.nix
+++ home/default.nix
@@ -1,6 +1,14 @@
-{ config, lib, pkgs, ... }
+{ ... }:
+
 {
-  imports = [
-  ];
-};
+  home = {
+    packages = with pkgs; [ #this is temporary
+      hello
+    ];
+    username = "tahlon";
+    homeDirectory = "/home/tahlon";
+    stateVersion = "24.05";
+  };
+  programs.home-manager.enable = true;
+}
 
