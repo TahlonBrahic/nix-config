2c8c5611b99f37500b8fba4bf45ebec9bbbef40e
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-09-02 00:00:00 +0000

    Go back to the basics

--- flake.nix
+++ flake.nix
@@ -40,5 +40,21 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: import ./outputs/default.nix { inherit inputs; };
+  #outputs = { self, nixpkgs, home-manager, ... }@inputs: import ./outputs/default.nix { inherit inputs; };
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      athena = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.tahlon = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
 }
