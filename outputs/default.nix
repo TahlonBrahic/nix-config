e5446769ebfb7b708478f99b0f1809d71481c952
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-09-02 00:00:00 +0000

    Add ability to define packages in src

--- outputs/default.nix
+++ outputs/default.nix
@@ -1,1 +1,24 @@
-import ./src/default.nix
+{ self, nixpkgs, ... } @ inputs: let
+  inherit (inputs.nixpkgs) lib;
+
+  tempArgs = inputs //
+    {
+      pkgs-unstable = import inputs.nixpkgs-unstable {
+        config.allowUnfree = true;
+      }; 
+      pkgs-stable = import inputs.nixpkgs {
+        config.allowUnfree = true;
+      };
+    };
+
+    args = {inherit inputs tempArgs};
+
+    systems = { 
+      import ./src (args // {system = "x86_64-linux";});
+    };
+
+    packages = x: lib.genAttrs (builtins.attrNames allSystems) (system: allSystems.${system}.packages or {} );
+    in
+    {
+      inherit packages;
+    }   
