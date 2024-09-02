1a4e8e6d4168d80fdbf667e035a40960f2797c53
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-09-02 17:01:55 -0500

    Add athena

--- outputs/default.nix
+++ outputs/default.nix
@@ -14,7 +14,7 @@
     args = {inherit inputs tempArgs; };
 
     systems = { 
-      import ./src (args // {system = "x86_64-linux";});
+      import ./src/athena.nix (args // {system = "x86_64-linux";});
     };
 
     packages = x: lib.genAttrs (builtins.attrNames allSystems) (system: allSystems.${system}.packages or {} );
