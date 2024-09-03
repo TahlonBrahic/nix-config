f36f7e56dfc5231348d8a7e89ffc6d95ffc7ab2a
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-09-02 19:01:39 -0500

    Add missing semi-colon in flake

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
 
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v0.4.1";
-      inputs.nixpkgs.follows = "nixpkgs"
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixos-generators = {
