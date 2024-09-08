9e42ebfab358bc3ec295e3694b9a38f10e60d76c
Tahlon Brahic <tahlonbrahic@proton.me>
2024-09-08 00:19:04 +0000

    Continue debugging:

--- outputs/systems/default.nix
+++ outputs/systems/default.nix
@@ -1,11 +1,11 @@
-{ lib, inputs, ... }:
+{ ... }@inputs:
 
 {
   source = builtins.filter (name: lib.strings.hasSuffix ".nix" name) (builtins.attrNames (builtins.readDir ./src));
   data = map (file: import (./src + "/" file) { inherit inputs; }) source;
-  dataWithoutPaths = builtins.attrValues (lib.attrsets.concatAttrs data);
+  dataWithoutPaths = builtins.attrValues data;
 
   outputs = {
-    nixosConfigurations = lib.attrsets.mergeAttrsList (map (it: it.nixosConfigurations or {}) dataWithoutPaths);
+    nixosConfigurations = inputs.lib.attrsets.mergeAttrsList (map (it: it.nixosConfigurations or {}) dataWithoutPaths);
   };
 }
