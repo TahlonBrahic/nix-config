4a69f28c2925733ade30bfc8e80b34d2e9b707d2
Tahlon Brahic <tahlonbrahic@proton.me>
2024-09-11 00:28:55 +0000

    Further debugging

--- outputs/systems/src/athena.nix
+++ outputs/systems/src/athena.nix
@@ -9,4 +9,11 @@
       	];
     };
   };
+  homeConfigurations = { 
+    "tahlon@athena" = inputs.home-manager.lib.homeManagerConfiguration {
+      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+      #extraSpecialArgs = { inherit inputs inputs.outputs; };
+      modules = [ ../../../users/i3t/home/nix ];
+      };
+  };
 }
