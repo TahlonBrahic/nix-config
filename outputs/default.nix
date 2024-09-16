377923ad6e2493824f37f1991609d67d883a4007
Tahlon Brahic <tahlonbrahic@proton.me>
2024-09-16 10:29:42 +0000

    Fix home-manager

--- outputs/default.nix
+++ outputs/default.nix
@@ -2,7 +2,6 @@
 
 let
   _ = builtins.trace "Inputs: ${inputs}" {};
-  inherit (inputs.home-manager) home-manager;
   inherit (inputs.nixpkgs) lib;
   inherit (inputs.self) self;
 
@@ -12,7 +11,7 @@ let
 #  customArgs = system: inputs # be sure to add customArgs to args below
 #     // { inherit bin vars; };
 
-  args = { inherit inputs home-manager lib self; };
+  args = { inherit inputs lib self; };
 
   systems = { x86_64-linux = import ./x86_64-linux/athena.nix args; };
   systemValues = builtins.attrValues systems;
