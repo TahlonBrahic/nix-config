9bd78be7fe7434b6c606ba25b63469511e4ec941
Tahlon Brahic <tahlonbrahic@proton.me>
2024-09-16 00:00:00 +0000

    Add nvchad4nix to inputs

--- flake.nix
+++ flake.nix
@@ -11,5 +11,9 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nvchad4nix = {
+      url = "github:nix-community/nix4nvchad";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 }
