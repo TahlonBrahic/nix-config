882eca9e4f3009c75fa8004884cdcebc1f287e51
Tahlon Brahic <tahlonbrahic@proton.me>
2024-09-16 00:00:00 +0000

    Create nani configuration

---
+++ hosts/nani/configuration.nix
@@ -0,0 +1,60 @@
+{ config, pkgs, ... }: 
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+ 
+  nix.settings.experimental-features = ["nix-command" "flakes" ];
+
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    wget
+    curl
+  ];
+
+  environment.variables.EDITOR = "vim";
+
+  nixpkgs = {
+   config = {
+      allowUnfree = true;
+    };
+  };
+
+  networking.hostName = "nani";
+
+  users.users = {
+    tahlon = {
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+      ];
+      extraGroups = ["wheel"];
+    };
+  };
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = true;
+    };
+  };
+
+  security.polkit.enable = true;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  services.qemuGuest.enable = true;
+  services.spice-vdagentd.enable = true;
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
+      }; 
+    };
+  };
+
+  system.stateVersion = "24.11";
+}
---
+++ hosts/nani/hardware-configuration.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/profiles/qemu-guest.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_blk" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/6dd73004-7b62-4fcb-adf3-7163bfe8dd2a";
+      fsType = "ext4";
+    };
+
+  swapDevices = [ ];
+
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
