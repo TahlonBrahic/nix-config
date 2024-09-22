1bed31ea4290ae6ad880055ce8896c4900cbca9b
Tahlon Brahic <tahlonbrahic@proton.me>
2024-09-22 23:54:59 +0000

    Add lib and vars to outputs

--- outputs/default.nix
+++ outputs/default.nix
@@ -3,8 +3,8 @@
 let
   inherit (inputs.nixpkgs) lib;
 
-  #bin = import ../bin { inherit lib; };
-  #vars = import ../vars { inherit lib; };
+  bin = import ../bin { inherit lib; };
+  vars = import ../vars { inherit lib; };
 
 #  customArgs = system: inputs # be sure to add customArgs to args below
 #     // { inherit bin vars; };
