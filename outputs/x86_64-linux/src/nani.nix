e5db9b22dfd8f57c1a020bde1a952ae11a60da19
Tahlon Brahic <tahlonbrahic@proton.me>
2024-09-23 00:00:00 +0000

    Starting templating

--- outputs/x86_64-linux/src/nani.nix
+++ outputs/x86_64-linux/src/nani.nix
@@ -1,23 +1,22 @@
-{ lib, inputs, disko, ... }@args:
+{ lib, inputs, system, customArgs, specialArgs ? (customArgs system), ... }:
 
 let
-  specialArgs = { inherit inputs; };
-  extraSpecialArgs = { inherit inputs; };
+  inherit (inputs) disko home-manager sops-nix;
   host = "nani"; 
 in
 {
   nixosConfigurations = {
     ${host} = lib.nixosSystem {
-      system = "x86_64-linux";
-        #inherit specialArgs;
+      inherit system;
+        inherit specialArgs;
       	modules = [
           ../../../hosts/${host}/configuration.nix
           inputs.disko.nixosModules.disko
-          inputs.sops-nix.nixosModules.sops
-	  inputs.home-manager.nixosModules.home-manager {
-            #inherit extraSpecialArgs;
+          sops-nix.nixosModules.sops
+	  home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
 	    home-manager.useUserPackages = true;
+	    home-manager.extraSpecialArgs = specialArgs;
             home-manager.users.tahlon.imports = [ ../../../hosts/${host}/home.nix ];  }
       ];
     };
