2697320d741862e48179bf2ac32fd40c7289b996
tahlon <tahlonbrahic@proton.me>
2024-09-30 03:18:51 +0000

    Debug the movement

--- modules/home/opt/i3.nix
+++ modules/home/opt/i3.nix
@@ -1,17 +1,17 @@
 { config, pkgs, ... }:
+
 { 
    services.xserver = {
      enable = true;
      layout = "us";
      xkbOptions = "terminate:ctrl_alt_bksp";
-   };
-   
-   services.xserver.windowManager.i3 = {
+     windowManager.i3 = {
      enable = true;
      package = pkgs.i3-gaps;
-   };
+     };
+  };
  
-   environment.systemPackages = with pkgs; [
+   home.packages = with pkgs; [
      i3status
      dmenu
      rofi
