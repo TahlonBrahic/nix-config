9bdb7ac71481ad8d077b510c5f5c6be324711719
tahlon <tahlonbrahic@proton.me>
2024-10-02 02:55:47 +0000

    Define fukushyuu in src

--- outputs/x86_64-linux/src/athena.nix
+++ outputs/x86_64-linux/src/athena.nix
@@ -1,22 +1,47 @@
-{ lib, inputs, ... }@args:
+{ inputs, lib, customLib, system, ... }@args:
 
 let
-  specialArgs = { inherit inputs; };
-  extraSpecialArgs = { inherit inputs; };
+  inherit (inputs) disko sops-nix;
+  inherit (customLib) relativeToRoot modulesRoot varsRoot systemTemplate;
+
+  # TODO: I would like to abstract how I import these as I do it for each host.
+  nixModules = map relativeToRoot [
+    "hosts/${hostName}/configuration.nix" 
+  ];
+
+  # TODO: I would like to abstract how I pass users to systemTemplate.
+  homeModules = with modulesRoot.home; [
+    base.archive
+    base.git
+    base.xdg
+    base.container
+    base.nix
+    base.home
+        
+    opt.obsidian
+    opt.sway
+    opt.kitty
+    opt.encryption
+    opt.gpg
+    opt.password
+    opt.zellij
+  ];
+  
+  hostName = "nani";
+  modules = {
+    nixos = [
+      disko.nixosModules.disko
+      sops-nix.nixosModules.sops
+    ] ++ nixModules;
+    home = [
+    ] ++ homeModules;
+  };
+  
+  tempVars = varsRoot.varsRoot.users.tahlon;
+
 in
 {
   nixosConfigurations = {
-    athena = lib.nixosSystem {
-      system = "x86_64-linux";
-        #inherit specialArgs;
-      	modules = [
-          ../../../hosts/athena/configuration.nix
-	  inputs.home-manager.nixosModules.home-manager {
-            #inherit extraSpecialArgs;
-            home-manager.useGlobalPkgs = true;
-	    home-manager.useUserPackages = true;
-            home-manager.users.tahlon.imports = [ ../../../hosts/athena/home.nix ];  }
-      ];
-    };
+    "${hostName}" = systemTemplate { inherit args modules; vars = tempVars; };
   };
 }
