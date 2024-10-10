b858b90d00b87ebfeb6f7e021653d31fdb55bed4
tahlon <tahlonbrahic@proton.me>
2024-10-10 00:00:00 +0000

    Add initialhashedpassword for Tahlon

--- vars/users/tahlon.nix
+++ vars/users/tahlon.nix
@@ -4,6 +4,6 @@
   username = "tahlon";
   name = "Tahlon Brahic";
   email = "tahlonbrahic@proton.me";
-  # initialHashedPassword = ?
-  # sshAuthorizedKeys = [ ? ];
+  initialHashedPassword = "$6$EkZtHt70BEv9.yW2$DUQk3jNS0IShl90xDcpb9kMHO7twHdB.RJE.r1KeXhKK33qTcd5WlyOnKM5fP4ArPazaVaS/FysKfUReKW7IA.";
+  sshAuthorizedKeys = [ ];
 }
