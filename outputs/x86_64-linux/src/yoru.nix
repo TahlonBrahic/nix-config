7062a6be50a111c44eae8b5e99703983e4ab57d8
tahlon <tahlonbrahic@proton.me>
2024-10-10 19:47:38 -0500

    Add yoru host

---
+++ outputs/x86_64-linux/src/yoru.nix
@@ -0,0 +1,42 @@
+{ inputs, lib, customLib, system, vars, ... }@args:
+
+let
+  inherit (inputs) disko sops-nix impermanence stylix;
+  inherit (customLib) relativeToRoot modulesRoot systemTemplate;
+
+  nixModules = with modulesRoot.nixos.opt; [
+    greetd
+    fhs
+    greetd
+  ];
+
+  homeModules = with modulesRoot.home; [
+    librewolf
+    encryption
+    fetch
+    hyprland
+    kitty
+    password
+    waybar
+    zellij
+  ];
+
+  modules = {
+    nixos = [
+      disko.nixosModules.disko
+      sops-nix.nixosModules.sops
+      stylix.nixosModules.stylix
+      impermanence.nixosModules.impermanence
+    ] ++ nixModules;
+    home = [
+    ] ++ homeModules;
+  };
+
+  outputVars = vars.users.tahlon // vars.hardware.yoru;
+
+in
+{
+  nixosConfigurations = {
+    "yoru" = systemTemplate { inherit args modules; vars = outputVars; };
+  };
+}
