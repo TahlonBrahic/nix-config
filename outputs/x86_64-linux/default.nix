bf20305eef2cbb0d20755c83ec4dc0f821def393
tahlon <tahlonbrahic@proton.me>
2024-10-14 00:00:00 +0000

    fix artifact in x86 default.nix

--- outputs/x86_64-linux/default.nix
+++ outputs/x86_64-linux/default.nix
@@ -1,19 +1,6 @@
-<<<<<<< HEAD
-{lib, ...} @ customArgs: let
-  inherit (customArgs.inputs) haumea;
-||||||| parent of 690d323 (Further debugging)
-{ lib, ... }@customArgs:
+{lib, inputs, ...} @ customArgs: let
 
-let
-  inherit (customArgs.inputs) haumea;
-=======
-{ lib, 
-  inputs,
-  ...
-}@customArgs:
-let
   inherit (inputs) haumea;
->>>>>>> 690d323 (Further debugging)
 
   data = haumea.lib.load {
     src = ./src;
