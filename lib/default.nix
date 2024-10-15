82499a4e14c18a5c4351a72283d5074b17676afb
tbrahic <tbrahic@rmsweb.com>
2024-10-15 06:47:22 -0500

    Start overlays

--- lib/default.nix
+++ lib/default.nix
@@ -23,6 +23,7 @@
   systemTemplate = import ./systemTemplate.nix;
   modulesRoot = import ./modulesRoot.nix {inherit haumea;};
   varsRoot = import ./varsRoot.nix {inherit haumea;};
+  overlaysRoot = import ./overlaysRoot.nix {inherit haumea;};
   baseNixosModules = import ./baseNixosModules.nix {inherit haumea;};
   baseHomeModules = import ./baseHomeModules.nix {inherit haumea;};
 }
---
+++ overlays/nvchad.nix
@@ -0,0 +1,6 @@
+{nvchad4nix, pkgs, ... }:
+(final: 
+  prev: {
+    nvchad = nvchad4nix.packages."${pkgs.system}".nvchad;
+   }
+)
