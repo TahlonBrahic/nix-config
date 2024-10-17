5edbb85001d2c9772af8961ae4f8be227b8fcaad
tbrahic <tbrahic@rmsweb.com>
2024-10-17 17:01:25 -0500

    Add pre-commit checks

---
+++ checks/pre-commit.nix
@@ -0,0 +1,18 @@
+{
+  pre-commit-hooks,
+  pkgs,
+  system,
+}:
+{
+  pre-commit-check = pre-commit-hooks.lib.${system}.run {
+    src = ./.;
+    default_stages = [ "pre-commit" ];
+    hooks = {
+      detect-private-keys.enable = true;
+      end-of-file-fixer.enable = true;
+      sort-file-contents.enable = true;
+      trim-trailing-whitespace.enable = true;
+      typos.enable = true;
+    };
+  };
+}
--- modules/home/opt/nvchad.nix
+++ modules/home/opt/nvchad.nix
@@ -7,5 +7,32 @@
   imports = [
     inputs.nvchad4nix.homeManagerModule
   ];
-  programs.nvchad.enable = true;
+  programs.nvchad = {
+    enable = true;
+    extraPackages = with pkgs; [
+
+      # LSPs
+      ansible-language-server
+      dockerfile-language-server-nodejs
+      haskell-language-server
+      yaml-language-server
+
+      # DAPs
+      vimPlugins.nvim-dap
+      vimPlugins.nvim-dap-ui
+      vimPlugins.nvim-dap-python
+      vimPlugins.nvim-dap-virtual-text
+
+      # Linters and Formatters
+      actionlint
+      ansible-lint
+      ast-grep
+      commitlint
+      gitlint
+      shellharden
+      textlint
+      yamllint
+
+    ];
+  };
 }
