0d0067e12c1083cd3e8776d516c369b6653aaddb
tahlon <tahlonbrahic@proton.me>
2024-10-17 20:39:00 -0500

    Start default flake shell

---
+++ shell.nix
@@ -0,0 +1,25 @@
+{
+  pkgs ? 
+    let
+      lock = (builtins.fromJSON (builtins.readFile ./flake.nix)).nodes.nixpkgs.locked;
+      nixpkgs = fetchTarball {
+        url = "https://github.com//nixos/nixpkgs/arhive/${lock.rev}.tar.gz";
+        sha256 = lock.narHash;
+      };
+    in 
+    import nixpkgs,
+  checks,
+  ...
+}:
+{
+  default = pkgs.mkShell {
+    NIX_CONFIG = "extra-experimental-features = nix-command flakes";
+    nativeBuildInputs = with pkgs; [
+      nix
+      home-manager
+      git 
+      sops
+      age
+    ];
+  };
+}
