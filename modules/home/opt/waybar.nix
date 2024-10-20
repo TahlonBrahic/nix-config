9d6b9e21d4a2d6c147f72ff708a38407d5838ae7
tahlon <tahlonbrahic@proton.me>
2024-10-20 12:56:20 -0500

    Add Hyprland keybinds and edit Waybar similar to Misterio77's config

--- modules/home/opt/waybar.nix
+++ modules/home/opt/waybar.nix
@@ -1,72 +1,349 @@
-{pkgs, ...}: let
-  logout = pkgs.writeShellScriptBin "logout" ''
-    if pgrep -x "wlogout" > /dev/null
-    then
-      pkill -x "wlogout"
-      exit 0
-     fi
-     wlogout --protocol layer-shell
-  '';
+{
+  outputs,
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}: let
+  commonDeps = with pkgs; [coreutils gnugrep systemd];
+  # Function to simplify making waybar outputs
+  mkScript = {
+    name ? "script",
+    deps ? [],
+    script ? "",
+  }:
+    lib.getExe (pkgs.writeShellApplication {
+      inherit name;
+      text = script;
+      runtimeInputs = commonDeps ++ deps;
+    });
+  # Specialized for JSON outputs
+  mkScriptJson = {
+    name ? "script",
+    deps ? [],
+    script ? "",
+    text ? "",
+    tooltip ? "",
+    alt ? "",
+    class ? "",
+    percentage ? "",
+  }:
+    mkScript {
+      inherit name;
+      deps = [pkgs.jq] ++ deps;
+      script = ''
+        ${script}
+        jq -cn \
+          --arg text "${text}" \
+          --arg tooltip "${tooltip}" \
+          --arg alt "${alt}" \
+          --arg class "${class}" \
+          --arg percentage "${percentage}" \
+          '{text:$text,tooltip:$tooltip,alt:$alt,class:$class,percentage:$percentage}'
+      '';
+    };
+
+  swayCfg = config.wayland.windowManager.sway;
+  hyprlandCfg = config.wayland.windowManager.hyprland;
 in {
-  home.packages = with pkgs; [
-    wlogout
-    wlr-randr
-    brightnessctl
-  ];
+  # Let it try to start a few more times
+  systemd.user.services.waybar = {
+    Unit.StartLimitBurst = 30;
+  };
+  programs.waybar = {
+    enable = true;
+    package = pkgs.waybar.overrideAttrs (oa: {
+      mesonFlags = (oa.mesonFlags or []) ++ ["-Dexperimental=true"];
+    });
+    systemd.enable = true;
+    settings = {
+      primary = {
+        exclusive = false;
+        passthrough = false;
+        height = 40;
+        margin = "6";
+        position = "top";
+        modules-left =
+          ["custom/menu"]
+          ++ (lib.optionals swayCfg.enable [
+            "sway/workspaces"
+            "sway/mode"
+          ])
+          ++ (lib.optionals hyprlandCfg.enable [
+            "hyprland/workspaces"
+            "hyprland/submap"
+          ])
+          ++ [
+            "custom/currentplayer"
+            "custom/player"
+          ];
 
-  programs = {
-    waybar = {
-      enable = true;
-      systemd.enable = true;
+        modules-center = [
+          "cpu"
+          "custom/gpu"
+          "memory"
+          "clock"
+          "custom/unread-mail"
+        ];
 
-      settings = {
-        mainBair = {
-          layer = "top";
-          position = "bottom";
-          height = 30;
-          margin-top = -10;
-          margin-bottom = 0;
-          margin-right = 100;
-          margin-left = 100;
-          font-family = "JetBrainsMono Nerd Font Mono";
+        modules-right = [
+          "tray"
+          "custom/rfkill"
+          "network"
+          "pulseaudio"
+          "battery"
+          "custom/hostname"
+        ];
 
-          window = {
-            border = 3;
-            titlebar = false;
+        clock = {
+          interval = 1;
+          format = "{:%d/%m %H:%M:%S}";
+          format-alt = "{:%Y-%m-%d %H:%M:%S %z}";
+          on-click-left = "mode";
+          tooltip-format = ''
+            <big>{:%Y %B}</big>
+            <tt><small>{calendar}</small></tt>'';
+        };
+
+        cpu = {
+          format = "  {usage}%";
+        };
+        "custom/gpu" = {
+          interval = 5;
+          exec = mkScript {script = "cat /sys/class/drm/card*/device/gpu_busy_percent | head -1";};
+          format = "󰒋  {}%";
+        };
+        memory = {
+          format = "  {}%";
+          interval = 5;
+        };
+
+        pulseaudio = {
+          format-source = "󰍬 {volume}%";
+          format-source-muted = "󰍭 0%";
+          format = "{icon} {volume}% {format_source}";
+          format-muted = "󰸈 0% {format_source}";
+          format-icons = {
+            default = [
+              "󰕿"
+              "󰖀"
+              "󰕾"
+            ];
           };
-          modules-left = ["custom/power"];
-          modules-center = ["clock"];
-          modules-right = ["custom/battery"];
-
-          "custom/power" = {
-            "format" = "{}";
-            "rotate" = 0;
-            "on-click" = "wlogout";
-            "interval" = 86400;
-            "tooltip" = true;
+          on-click = lib.getExe pkgs.pavucontrol;
+        };
+        idle_inhibitor = {
+          format = "{icon}";
+          format-icons = {
+            activated = "󰒳";
+            deactivated = "󰒲";
           };
-
-          "custom/battery" = {
-            "states" = {
-              "good" = 80;
-              "warning" = 30;
-              "critical" = 15;
-            };
-            "format" = "{icon} {capacity}%";
-            "format-charging" = " {capacity}%";
-            "format-plugged" = " {capacity}%";
-            "format-alt" = "{time} {icon}";
-            "format-icons" = ["󰂎" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹"];
+        };
+        battery = {
+          bat = "BAT0";
+          interval = 10;
+          format-icons = [
+            "󰁺"
+            "󰁻"
+            "󰁼"
+            "󰁽"
+            "󰁾"
+            "󰁿"
+            "󰂀"
+            "󰂁"
+            "󰂂"
+            "󰁹"
+          ];
+          format = "{icon} {capacity}%";
+          format-charging = "󰂄 {capacity}%";
+          onclick = "";
+        };
+        "sway/window" = {
+          max-length = 20;
+        };
+        network = {
+          interval = 3;
+          format-wifi = "   {essid}";
+          format-ethernet = "󰈁 Connected";
+          format-disconnected = "";
+          tooltip-format = ''
+            {ifname}
+            {ipaddr}/{cidr}
+            Up: {bandwidthUpBits}
+            Down: {bandwidthDownBits}'';
+        };
+        "custom/menu" = {
+          interval = 1;
+          return-type = "json";
+          exec = mkScriptJson {
+            deps = lib.optional hyprlandCfg.enable hyprlandCfg.package;
+            text = "";
+            tooltip = ''$(grep PRETTY_NAME /etc/os-release | cut -d '"' -f2)'';
+            class = let
+              isFullScreen =
+                if hyprlandCfg.enable
+                then "hyprctl activewindow -j | jq -e '.fullscreen' &>/dev/null"
+                else "false";
+            in "$(if ${isFullScreen}; then echo fullscreen; fi)";
           };
-          style = ''
-            #custom-hyprbindings, #battery, #powery {
-              padding: 0px 18px;
-              margin: 4px 0px;
-              border-radius: 10px 24px 10px 24px;
-            }
-          '';
+        };
+        "custom/hostname" = {
+          exec = mkScript {script = ''
+            echo "$USER@$HOSTNAME"
+          '';};
+          on-click = mkScript {script = ''
+            systemctl --user restart waybar
+          '';};
+        };
+        "custom/unread-mail" = {
+          interval = 5;
+          return-type = "json";
+          exec = mkScriptJson {
+            deps = [pkgs.findutils pkgs.procps];
+            script = ''
+              count=$(find ~/Mail/*/Inbox/new -type f | wc -l)
+              if pgrep mbsync &>/dev/null; then
+                status="syncing"
+              else
+                if [ "$count" == "0" ]; then
+                  status="read"
+                else
+                  status="unread"
+                fi
+              fi
+            '';
+            text = "$count";
+            alt = "$status";
+          };
+          format = "{icon}  ({})";
+          format-icons = {
+            "read" = "󰇯";
+            "unread" = "󰇮";
+            "syncing" = "󰁪";
+          };
+        };
+        "custom/currentplayer" = {
+          interval = 2;
+          return-type = "json";
+          exec = mkScriptJson {
+            deps = [pkgs.playerctl];
+            script = ''
+              all_players=$(playerctl -l 2>/dev/null)
+              selected_player="$(playerctl status -f "{{playerName}}" 2>/dev/null || true)"
+              clean_player="$(echo "$selected_player" | cut -d '.' -f1)"
+            '';
+            alt = "$clean_player";
+            tooltip = "$all_players";
+          };
+          format = "{icon}{}";
+          format-icons = {
+            "" = " ";
+            "Celluloid" = "󰎁 ";
+            "spotify" = "󰓇 ";
+            "ncspot" = "󰓇 ";
+            "qutebrowser" = "󰖟 ";
+            "firefox" = " ";
+            "discord" = " 󰙯 ";
+            "sublimemusic" = " ";
+            "kdeconnect" = "󰄡 ";
+            "chromium" = " ";
+          };
+        };
+        "custom/player" = {
+          exec-if = mkScript {
+            deps = [pkgs.playerctl];
+            script = ''
+              selected_player="$(playerctl status -f "{{playerName}}" 2>/dev/null || true)"
+              playerctl status -p "$selected_player" 2>/dev/null
+            '';
+          };
+          exec = mkScript {
+            deps = [pkgs.playerctl];
+            script = ''
+              selected_player="$(playerctl status -f "{{playerName}}" 2>/dev/null || true)"
+              playerctl metadata -p "$selected_player" \
+                --format '{"text": "{{artist}} - {{title}}", "alt": "{{status}}", "tooltip": "{{artist}} - {{title}} ({{album}})"}' 2>/dev/null
+            '';
+          };
+          return-type = "json";
+          interval = 2;
+          max-length = 30;
+          format = "{icon} {}";
+          format-icons = {
+            "Playing" = "󰐊";
+            "Paused" = "󰏤 ";
+            "Stopped" = "󰓛";
+          };
+          on-click = mkScript {
+            deps = [pkgs.playerctl];
+            script = "playerctl play-pause";
+          };
+        };
+        "custom/rfkill" = {
+          interval = 1;
+          exec-if = mkScript {
+            deps = [pkgs.util-linux];
+            script = "rfkill | grep '\<blocked\>'";
+          };
         };
       };
     };
+    # Cheatsheet:
+    # x -> all sides
+    # x y -> vertical, horizontal
+    # x y z -> top, horizontal, bottom
+    # w x y z -> top, right, bottom, left
+    style = 
+      ''
+        * {
+          font-size: 12pt;
+          padding: 0;
+          margin: 0 0.4em;
+        }
+
+        window#waybar {
+          padding: 0;
+          border-radius: 0.5em;
+        }
+        .modules-left {
+          margin-left: -0.65em;
+        }
+        .modules-right {
+          margin-right: -0.65em;
+        }
+
+        #workspaces button {
+          padding-left: 0.4em;
+          padding-right: 0.4em;
+          margin-top: 0.15em;
+          margin-bottom: 0.15em;
+        }
+        #clock {
+          padding-right: 1em;
+          padding-left: 1em;
+          border-radius: 0.5em;
+        }
+
+        #custom-menu {
+          padding-right: 1.5em;
+          padding-left: 1em;
+          margin-right: 0;
+          border-radius: 0.5em;
+        }
+        #custom-hostname {
+          padding-right: 1em;
+          padding-left: 1em;
+          margin-left: 0;
+          border-radius: 0.5em;
+        }
+        #custom-currentplayer {
+          padding-right: 0;
+        }
+        #custom-gpu, #cpu, #memory {
+          margin-left: 0.05em;
+          margin-right: 0.55em;
+        }
+      '';
   };
 }
