606c22f682cdc30fb6b22373dd546b7640035f9b
tahlon <tahlonbrahic@proton.me>
2024-10-22 00:00:00 +0000

    night night

--- outputs/default.nix
+++ outputs/default.nix
@@ -3,16 +3,21 @@
   haumea,
   ...
 } @ inputs: let
-
   supportedSystems = ["x86_64-linux" "aarch64-linux"];
 
   forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-  systems = forAllSystems (system: import ./${system} customArgs);
+  systems = forAllSystems (system: let 
+    customArgs = { 
+      inherit inputs vars system; 
+      inherit (lib.${system}) lib;
+    }; 
+  in 
+    import ./${system} customArgs);
 
   systemValues = builtins.attrValues systems;
 
-  packages = forAllSystems (system: let pkgs = nixpkgs.legacyPackages.${system}; in pkgs );
+  packages = forAllSystems (system: let pkgs = nixpkgs.legacyPackages.${system}; in pkgs);
 
   pkgs = forAllSystems (system: systems.${system}.packages or {});
 
@@ -20,9 +25,7 @@
 
   vars = lib.vars;
 
-  lib = nixpkgs.lib // forAllSystems (system: import ../lib {inherit inputs pkgs;});
-
-  customArgs = forAllSystems (system: let customArgs = { inherit inputs lib vars system; }; in customArgs );
+  lib = forAllSystems (system: import ../lib {inherit inputs haumea pkgs;}) // nixpkgs.lib;
 
   overlays = forAllSystems (system: import overrides {inherit inputs system;});
 
