cb73c01bf3ca43bb00aecbcecf84a70fd5e66745
TahlonBrahic <tahlonbrahic@gmail.com>
2024-10-26 00:00:00 +0000

    Edit shell

--- shell.nix
+++ shell.nix
@@ -1,22 +1,28 @@
-{
-  pkgs ? 
-    let
-      lock = (builtins.fromJSON (builtins.readFile ./flake.nix)).nodes.nixpkgs.locked;
-      nixpkgs = fetchTarball {
-        url = "https://github.com//nixos/nixpkgs/arhive/${lock.rev}.tar.gz";
-        sha256 = lock.narHash;
-      };
-    in 
-    import nixpkgs,
-  ...
-}:
-{
+{pkgs ? let
+  lock = (builtins.fromJSON (builtins.readFile ./flake.nix)).nodes.nixpkgs.locked;
+  nixpkgs = fetchTarball {
+    url = "https://github.com//nixos/nixpkgs/arhive/${lock.rev}.tar.gz";
+    sha256 = lock.narHash;
+  };
+in
+  import nixpkgs, ...}: {
   default = pkgs.mkShell {
     NIX_CONFIG = "extra-experimental-features = nix-command flakes";
     nativeBuildInputs = with pkgs; [
+      # Nix
       nix
       home-manager
-      git 
+      git
+
+      # Formatters & Linters
+      bashInteractive
+      alejandra
+      deadnix
+      statix
+      typos
+      nodePackages.prettier
+
+      # Secrets Management
       sops
       age
     ];
