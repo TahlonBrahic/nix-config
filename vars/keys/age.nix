3eb041ad2aa88459f0b7b48af96ee0771416bb9f
TahlonBrahic <tahlonbrahic@gmail.com>
2024-10-31 20:18:34 -0500

    add public keys

---
+++ vars/keys/age.nix
@@ -0,0 +1,3 @@
+{
+  "shilo" = "age1mm7xrs8rh0hllmw689406ah358tl9rjftedy6xu50w4qyyjwnsksnx6vu3";
+}
---
+++ vars/keys/ssh.nix
@@ -0,0 +1,3 @@
+{
+  "shilo" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICl40SMOUGnodt39ZLHoLt9trFZfv3rStO+j3bc3tdxf";
+}
