2355df907a1fb81e2ad64c6cba8367e74d2bcdfa
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-01 00:00:00 +0000

    Add service to lock upon yubikey withdrawal

--- modules/nixos/base/yubikey.nix
+++ modules/nixos/base/yubikey.nix
@@ -14,6 +14,14 @@
     yubikey-agent.enable = true;
     udev = {
       packages = with pkgs; [yubikey-personalization];
+      extraRules = ''
+        ACTION=="remove",\
+         ENV{ID_BUS}=="usb",\
+         ENV{ID_MODEL_ID}=="0407",\
+         ENV{ID_VENDOR_ID}=="1050",\
+         ENV{ID_VENDOR}=="Yubico",\
+         RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+      '';
     };
   };
 
