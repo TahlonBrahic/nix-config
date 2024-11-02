044dd1294bf032813621a3c456fd2066a6beb34a
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-02 00:03:44 -0500

    Fix devTools and adjust wayland packages

--- modules/home/base/devTools.nix
+++ modules/home/base/devTools.nix
@@ -9,6 +9,7 @@
     httpie
     ansible
     tree-sitter
+    any-nix-shell
 
     # Language Servers
     nil
@@ -44,32 +45,32 @@
       enableFishIntegration = true;
       enableZshIntegration = true;
     };
-  };
-  lsd = {
-    enable = true;
-    enableAliases = true;
-  };
-  starship = {
-    enable = true;
-    settings = {
-      aws.disabled = true;
-      gcloud.disabled = true;
-      kubernetes.disabled = false;
-      git_branch.style = "242";
-      directory = {
-        style = "blue";
-        truncate_to_repo = false;
-        truncation_length = 8;
-      };
-      python.disabled = true;
-      ruby.disabled = true;
-      hostname.ssh_only = false;
-      hostname.style = "bold green";
+    lsd = {
+      enable = true;
+      enableAliases = true;
     };
-  };
-  zoxide = {
-    enable = true;
-    enableFishIntegration = true;
-    options = ["--cmd cd"];
+    starship = {
+      enable = true;
+      settings = {
+        aws.disabled = true;
+        gcloud.disabled = true;
+        kubernetes.disabled = false;
+        git_branch.style = "242";
+        directory = {
+          style = "blue";
+          truncate_to_repo = false;
+          truncation_length = 8;
+        };
+        python.disabled = true;
+        ruby.disabled = true;
+        hostname.ssh_only = false;
+        hostname.style = "bold green";
+      };
+    };
+    zoxide = {
+      enable = true;
+      enableFishIntegration = true;
+      options = ["--cmd cd"];
+    };
   };
 }
