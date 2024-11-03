3e240f3eeac1459e386b7c7edce3e593859a2bc5
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-03 00:00:00 +0000

    Transition modules to proper module system

--- vars/monitors/laptop.nix
+++ vars/monitors/laptop.nix
@@ -1,3 +1,13 @@
 {
+  WAYLAND_DISPLAY = "wayland-1";
+  ASPECT_RATIO = "16:10";
 
+  monitor = {
+    "eDP-1" = {
+      position = "0:0"; # Position on the virtual screen
+      resolution = "2880x1800"; # Resolution
+      scale = 2.0; # Scale factor
+      refreshRate = 120; # Refresh rate if applicable
+    };
+  };
 }
