3287b522fe181d66d7338428c6e91d9ec3f7485a
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-07 20:34:44 -0600

    Add secrets for taistealai

--- vars/keys/ssh.nix
+++ vars/keys/ssh.nix
@@ -1,3 +1,4 @@
 {
   "shilo" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICl40SMOUGnodt39ZLHoLt9trFZfv3rStO+j3bc3tdxf";
+  "taistealai" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH0NCg7dOAIc9pRDUb3sOuBluzZHuQkUoYlrQAce/Y89 taistealai";
 }
