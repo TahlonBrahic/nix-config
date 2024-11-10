cbda4be0e4a677064c1a65e228ab48bb1714c993
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-10 11:19:59 -0600

    Update nixvim to module

--- modules/home/opt/nixvim.nix
+++ modules/home/opt/nixvim.nix
@@ -1,9 +1,25 @@
-{pkgs, ...}:
 {
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  cfg = config.fuyuEditor;
+in {
   options = {
+    fuyuEditor = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+      };
+      defaultEditor = lib.mkOption {
+        type = lib.types.enum [pkgs.fuyuvim pkgs.jeezyivm pkgs.nvchad pkgs.nvim];
+        default = pkgs.fuyuvim;
+      };
+    };
   };
 
-  config = {
-    home.packages = with pkgs; [ fuyuvim ];
+  config = lib.mkIf cfg.enable {
+    home.packages = cfg.defaultEditor;
   };
-} 
+}
