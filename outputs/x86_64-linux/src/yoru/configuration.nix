68a090ea74836ff8c7dc512da34261d031e6eb9e
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-19 20:06:30 -0600

    Create starter config to test kosei

--- outputs/x86_64-linux/src/yoru/configuration.nix
+++ outputs/x86_64-linux/src/yoru/configuration.nix
@@ -1,4 +1,14 @@
-{inputs, ...}: {
+{
+  imports = [
+    ./home.nix
+    ./hardware-configuration.nix
+  ];
+
   config = {
+    fuyuNoKosei = {
+      compositor.hyprland.enable = true;
+      services.laptop.enable = true;
+      yubikey.enable = true;
+    };
   };
 }
