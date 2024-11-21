7024706dc899f84eb17666a0edb76dc196e7991e
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-20 21:17:04 -0600

    add src directories

--- flake.nix
+++ flake.nix
@@ -1,28 +1,27 @@
 {
   description = "NixOS configuration that follows fuyu-no-kosei.";
 
-  inputs.url = "github:TahlonBrahic/fuyu-no-kosei";
+  inputs.fuyuNoKosei.url = "github:TahlonBrahic/fuyu-no-kosei";
 
-  outputs = {inputs}: let
-    inherit (inputs) nixpkgs systems pkgs lib;
+  outputs = inputs: let
+    inherit (inputs) fuyuNoKosei;
+    inherit (fuyuNoKosei) pkgs forEachSystem genNixosConfig;
 
-    forEachSystem = nixpkgs.lib.genAttrs systems;
     systemConfigurations = forEachSystem (system: import ./outputs/${system} args.${system});
     systemValues = builtins.attrValues systemConfigurations;
 
     args = forEachSystem (system: rec {
-      inherit inputs system;
+      inherit (fuyuNoKosei) inputs;
+      inherit system;
       inherit (pkgs.${system}) pkgs;
-      inherit (lib.${system}) lib;
+      lib = fuyuNoKosei.lib.${system};
     });
 
     formatter = forEachSystem (system: pkgs.${system}.alejandra);
 
     devShells = forEachSystem (system: import ./shell.nix {inherit (pkgs.${system}) pkgs;});
 
-    nixosConfigurations =
-      nixpkgs.lib.attrsets.mergeAttrsList
-      (map (it: it.nixosConfigurations or {}) systemValues);
+    nixosConfigurations = genNixosConfig (map (it: it.nixosConfigurations or {}) systemValues);
   in {
     inherit nixosConfigurations devShells formatter;
   };
