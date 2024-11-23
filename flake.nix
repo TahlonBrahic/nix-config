0834eb342d5253b0b131f2e935f11541e2df949c
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-22 20:08:35 -0600

    almost done with system template

--- flake.nix
+++ flake.nix
@@ -5,13 +5,13 @@
 
   outputs = inputs: let
     inherit (inputs) fuyuNoKosei;
-    inherit (fuyuNoKosei) pkgs forEachSystem genNixosConfig;
+    inherit (fuyuNoKosei) pkgs forEachSystem genNixosConfig nixosModules homeManagerModules;
 
     systemConfigurations = forEachSystem (system: import ./outputs/${system} args.${system});
     systemValues = builtins.attrValues systemConfigurations;
 
     args = forEachSystem (system: rec {
-      inherit (fuyuNoKosei) inputs;
+      inputs = fuyuNoKosei.inputs // {inherit nixosModules homeManagerModules;};
       inherit system;
       inherit (pkgs.${system}) pkgs;
       lib = fuyuNoKosei.lib.${system};
