b107089e286dcc7d85893794056c04046ee8d47a
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-27 00:00:00 +0000

    adjust

--- outputs/x86_64-linux/systems/yoru/_configuration.nix
+++ outputs/x86_64-linux/systems/yoru/_configuration.nix
@@ -1,6 +1,8 @@
 {
-  services.laptop.enable = true;
-  yubikey.enable = true;
-  secrets.defaultSopsFile = ../../../../secrets/secrets.yaml;
-  displayManager.option = "tuigreet";
+  config = {
+    services.laptop.enable = true;
+    yubikey.enable = true;
+    secrets.defaultSopsFile = ../../../../secrets/secrets.yaml;
+    displayManager = {option = "tuigreet";};
+  };
 }
