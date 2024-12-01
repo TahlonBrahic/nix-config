963b4f013ed237106d0b4f15404db768d7a56eed
Tahlon <tahlobrahic@proton.me>
2024-12-01 00:00:00 +0000

    switch to lightdm and add obs-studio to user config

--- outputs/x86_64-linux/systems/fukushyuu/_configuration.nix
+++ outputs/x86_64-linux/systems/fukushyuu/_configuration.nix
@@ -3,5 +3,9 @@
     secrets.defaultSopsFile = ../../../../secrets/secrets.yaml;
     GNOME.enable = true;
     gaming.enable = true;
+    displayManager = {
+      tuigreet.enable = false;
+      lightdm.enable = true;
+    };
   };
 }
