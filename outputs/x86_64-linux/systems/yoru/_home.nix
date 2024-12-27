516c36a0fbd95e4d8a6b3ca2580e51e0adcc9ab6
TahlonBrahic <tahlonbrahic@gmail.com>
2024-12-27 00:00:00 +0000

    add flake-parts to flake.nix

--- outputs/x86_64-linux/systems/yoru/_home.nix
+++ outputs/x86_64-linux/systems/yoru/_home.nix
@@ -1,6 +1,12 @@
 {
   home-manager.users."tahlon".fuyuNoKosei = {
+    browser.firefox.enable = true;
     fileManager.enable = true;
+    git = {
+      enable = true;
+      userName = "Tahlon Brahic";
+      userEmail = "tahlonbrahic@proton.me";
+    };
     homePackages.enable = true;
     hyprland.enable = true;
     passwordManagement.enable = true;
