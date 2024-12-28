2f760e9fd02009a8807d26c02f546ea6becb4479
TahlonBrahic <tahlonbrahic@gmail.com>
2024-12-27 21:41:23 -0600

    add flake-parts to flake.nix

--- flake.nix
+++ flake.nix
@@ -3,10 +3,9 @@
 
   inputs.fuyuNoKosei.url = "github:TahlonBrahic/fuyu-no-kosei";
 
-  outputs = inputs: let
-    inherit (inputs) fuyuNoKosei;
-    inherit (fuyuNoKosei) pkgs forEachSystem genConfig nixosModules homeManagerModules;
-
+  outputs = inputs @ {fuyuNoKosei, ...}: let
+    inherit (fuyuNoKosei) systems pkgs forEachSystem genConfig nixosModules homeManagerModules;
+    inherit (fuyuNoKosei.inputs) flake-parts self;
     systemConfigurations = forEachSystem (system: import ./outputs/${system} args.${system});
     systemValues = builtins.attrValues systemConfigurations;
 
@@ -16,14 +15,16 @@
       pkgs = fuyuNoKosei.pkgs.${system};
       lib = fuyuNoKosei.lib.${system};
     });
-
-    formatter = forEachSystem (system: pkgs.${system}.alejandra);
-
-    devShells = forEachSystem (system: import ./shell.nix {inherit (pkgs.${system}) pkgs;});
-
-    nixosConfigurations = genConfig (map (it: it.nixosConfigurations or {}) systemValues);
-    nixOnDroidConfigurations = genConfig (map (it: it.nixOnDroidConfigurations or {}) systemValues);
-  in {
-    inherit nixosConfigurations nixOnDroidConfigurations devShells formatter;
-  };
+  in
+    flake-parts.lib.mkFlake {inherit inputs self;} {
+      inherit systems;
+      flake = {
+        debug = true;
+        nixosConfigurations = genConfig (map (it: it.nixosConfigurations or {}) systemValues);
+        nixOnDroidConfigurations = genConfig (map (it: (it.nixosConfigurations or {}) systemValues));
+        formatter = forEachSystem (system: pkgs.${system}.alejandra);
+        devShells = forEachSystem (system: import ./shell.nix {inherit (pkgs.${system}) pkgs;});
+        inherit forEachSystem;
+      };
+    };
 }
