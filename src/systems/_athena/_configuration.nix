51909dd4ce7f94bb8954423cf8d73ae1a96f055d
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-11 00:00:00 +0000

    update all hosts to work again

--- src/systems/_athena/_configuration.nix
+++
@@ -1,9 +0,0 @@
-{
-  fuyuNoKosei = {
-    boot.enable = true;
-    displayManager.enable = true;
-    syncthing.enable = true;
-    secrets.defaultSopsFile = ../../../../secrets/secrets.yaml;
-    ssh.enable = true;
-  };
-}
--- src/systems/yoru/yoru.nix
+++ src/systems/yoru/yoru.nix
@@ -4,7 +4,6 @@
   pkgs,
   ...
 }: let
-  inherit (inputs) kosei home-manager;
   hostName = "yoru";
   users = ["tahlon"];
   system = "x86_64-linux";
@@ -13,32 +12,5 @@
     ./_configuration.nix
     ./_hardware-configuration.nix
   ];
-  specialArgs = {inherit inputs hostName pkgs system users;};
 in
-  lib.nixosSystem {
-    inherit system specialArgs;
-    modules =
-      [
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            backupFileExtension = "bak";
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            extraSpecialArgs = specialArgs;
-            # Iterates over a list of users provided in the function call
-            users = inputs.nixpkgs.lib.attrsets.genAttrs users (user: {
-              imports =
-                lib.forEach
-                (builtins.attrNames kosei.modules.home)
-                (module: builtins.getAttr module kosei.modules.home);
-              config.home.username = user;
-            });
-          };
-        }
-      ]
-      ++ extraModules
-      ++ lib.forEach
-      (builtins.attrNames kosei.modules.nixos)
-      (module: builtins.getAttr module kosei.modules.nixos);
-  }
+  inputs.kosei.lib.systemTemplate {inherit extraModules hostName inputs lib pkgs system users;}
