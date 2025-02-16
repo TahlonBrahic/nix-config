384adcf9568d35cf1d69aba9c77390de456a7899
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-16 12:44:13 -0600

    update flake inputs / flake to accept flake-parts kosei

--- src/systems/yoru/_configuration.nix
+++ src/systems/yoru/_configuration.nix
@@ -15,6 +15,7 @@
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDRUJCFyU2Bhag5GHGq2ihZL6LljX8EZygeKU6KDzHL8 tbrahic@proton.me"
         ];
       };
+      torrenting.enable = true;
       yubikey.enable = true;
       design = {
         theme = "${inputs.assets}/themes/nord.yaml";
